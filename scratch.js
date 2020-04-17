3c77b6c82a3bab0bc06e44f070b98e3992430a9b
julieritz <60047114+julieritz@users.noreply.github.com>
2020-04-16 18:43:28 -0700

    Working JS

---
+++ scratch.js
@@ -0,0 +1,187 @@
+// var array = [beef, chicken, fish, tofu]
+// console.log(array)
+
+// var resultsArr = []
+
+// var beef = array[0]
+// var chicken = array[1]
+// var tofu = array[2]
+// var fish = array[3]
+
+// var yes1 = 0
+// var no1 = 0
+// var yes2 = 0
+// var no2 = 0
+// var yes3 = 0
+// var no3 = 0
+// var yes4 = 0
+// var no4 = 0
+// var yes5 = 0
+// var no5 = 0
+// var yes6 = 0
+// var no6 = 0
+// var yes7 = 0
+// var no7 = 0
+// var yes8 = 0
+// var no8 = 0
+// var yes9 = 0
+// var no9 = 0
+
+// console.log(yes1)
+// console.log(no1)
+// console.log(yes2)
+// console.log(no2)
+
+// function scoreY1(yes1) {
+//     // beef--
+//     // chicken--
+//     // tofu++
+//     // veggies++
+//     // vegan++
+//     console.log(yes1)
+
+//     if (yes1) {
+//         resultsArr[0] = "meat"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[0] = "noMeat"
+//         console.log(resultsArr)
+//     }
+
+//     return resultsArr[0]
+
+// }
+
+// function scoreN1(no1) {
+//     console.log(no1)
+
+//     if (no1) {
+//         resultsArr[0] = "noMeat"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[0] = "meat"
+//         console.log(resultsArr)
+//     }
+
+//     return resultsArr[0]
+
+// }
+
+// function scoreY2(yes2) {
+//     console.log(yes2)
+
+//     if (yes2) {
+//         resultsArr[1] = "spicy"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[1] = "noSpicy"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreN2(no2) {
+//     console.log(no2)
+
+//     if (no2) {
+//         resultsArr[1] = "noSpicy"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[1] = "spicy"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreY3(yes3) {
+//     console.log(yes3)
+
+//     if (yes3) {
+//         resultsArr[2] = "strong"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[2] = "noStrong"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreN3(no3) {
+//     console.log(no3)
+
+//     if (no3) {
+//         resultsArr[2] = "noStrong"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[2] = "strong"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreY4(yes4) {
+//     console.log(yes4)
+
+//     if (yes4) {
+//         resultsArr[3] = "sweet"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[3] = "noSweet"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreN4(no4) {
+//     console.log(no4)
+
+//     if (no4) {
+//         resultsArr[3] = "noSweet"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[3] = "sweet"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreY5(yes5) {
+//     console.log(yes5)
+
+//     if (yes5) {
+//         resultsArr[4] = "dance"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[4] = "noDance"
+//         console.log(resultsArr)
+//     }
+
+//     return (resultsArr)
+// }
+
+// function scoreN5(no5) {
+//     console.log(no5)
+
+//     if (no5) {
+//         resultsArr[4] = "noDance"
+//         console.log(resultsArr)
+//     }
+//     else {
+//         resultsArr[4] = "dance"
+//         console.log(resultsArr)
+//     }
--- script.js
+++ script.js
@@ -1,287 +1,73 @@
-var array = [beef, chicken, tofu, veggies, vegan]
-console.log(array)
+var chosenIngredient;
+var chosenAlcohol;
 
-var resultsArr = []
+var foodArray = []
+var beef = 0
+var chicken = 0
+var tofu = 0
+var fish = 0
 
-var beef = array[0]
-var chicken = array[1]
-var tofu = array[2]
-var fish = array[3]
 
-var yes1 = 0
-var no1 = 0
-var yes2 = 0
-var no2 = 0
-var yes3 = 0
-var no3 = 0
-var yes4 = 0
-var no4 = 0
-var yes5 = 0
-var no5 = 0
-var yes6 = 0
-var no6 = 0
-var yes7 = 0
-var no7 = 0
-var yes8 = 0
-var no8 = 0
-var yes9 = 0
-var no9 = 0
+$("#resultsCalc").on("click", function () {
+    for (var i = 0; i < foodArray.length; ++i) {
+        if (foodArray[i] === 'beef') {
+            beef++;
+        }
+        if (foodArray[i] === 'chicken') {
+            chicken++;
+        }
+        if (foodArray[i] === 'fish') {
+            fish++;
+        }
+        if (foodArray[i] === 'tofu') {
+            tofu++;
+        }
+    }
+    winnerPick();
+    fetchRecipes([chosenIngredient])
+        .then(function (response) {
+            jsonResponse = JSON.parse(response)
+            console.log(jsonResponse)
+            foodRandom = Math.floor(Math.random() * jsonResponse.results.length)
+            console.log(jsonResponse.results[foodRandom])
+            $("#foodImg").attr("src", jsonResponse.results[foodRandom].thumbnail);
+            $("#foodTitle").text(jsonResponse.results[foodRandom].title);
+            $("#foodLink").text(jsonResponse.results[foodRandom].ingredients);
+        })
+    GetDrink()
+})
 
-console.log(yes1)
-console.log(no1)
-console.log(yes2)
-console.log(no2)
-
-function scoreY1(yes1) {
-    // beef--
-    // chicken--
-    // tofu++
-    // veggies++
-    // vegan++
-    console.log(yes1)
-
-    if (yes1) {
-        resultsArr[0] = "meat"
-        console.log(resultsArr)
+function winnerPick() {
+    if (beef > chicken && beef > fish && beef > tofu) {
+        chosenIngredient = "beef"
+        chosenAlcohol = "tequila"
+    }
+    else if (chicken > beef && chicken > fish && chicken > tofu) {
+        chosenIngredient = "chicken"
+        chosenAlcohol = "rum"
+    }
+    else if (fish > chicken && fish > beef && fish > tofu) {
+        chosenIngredient = "fish"
+        chosenAlcohol = "vodka"
+    }
+    else if (tofu > chicken && tofu > beef && tofu > fish) {
+        chosenIngredient = "tofu"
+        chosenAlcohol = "gin"
     }
     else {
-        resultsArr[0] = "noMeat"
-        console.log(resultsArr)
-    }
-
-    return resultsArr[0]
-
-}
-
-function scoreN1(no1) {
-    console.log(no1)
-
-    if (no1) {
-        resultsArr[0] = "noMeat"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[0] = "meat"
-        console.log(resultsArr)
-    }
-
-    return resultsArr[0]
-    
-}
-
-function scoreY2(yes2) {
-    console.log(yes2)
-
-    if (yes2) {
-        resultsArr[1] = "spicy"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[1] = "noSpicy"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreN2(no2) {
-    console.log(no2)
-
-    if (no2) {
-        resultsArr[1] = "noSpicy"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[1] = "spicy"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreY3(yes3) {
-    console.log(yes3)
-    
-    if (yes3) {
-        resultsArr[2] = "strong"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[2] = "noStrong"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreN3(no3) {
-    console.log(no3)
-
-    if (no3) {
-        resultsArr[2] = "noStrong"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[2] = "strong"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreY4(yes4) {
-    console.log(yes4)
-    
-    if (yes4) {
-        resultsArr[3] = "sweet"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[3] = "noSweet"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreN4(no4) {
-    console.log(no4)
-
-    if (no4) {
-        resultsArr[3] = "noSweet"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[3] = "sweet"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreY5(yes5) {
-    console.log(yes5)
-    
-    if (yes5) {
-        resultsArr[4] = "dance"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[4] = "noDance"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreN5(no5) {
-    console.log(no5)
-
-    if (no5) {
-        resultsArr[4] = "noDance"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[4] = "dance"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreY6(yes6) {
-    console.log(yes6)
-    
-    if (yes6) {
-        resultsArr[5] = "modern"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[5] = "noModern"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-function scoreN6(no6) {
-    console.log(no6)
-
-    if (no6) {
-        resultsArr[5] = "noModern"
-        console.log(resultsArr)
-    }
-    else {
-        resultsArr[5] = "modern"
-        console.log(resultsArr)
-    }
-
-    return (resultsArr)
-}
-
-var testArray1 = ["beef", "chicken", "beef", "lamb"]
-var testArray2 = ["chicken", "beef", "chicken","lamb"]
-var testArray3 = ["lamb", "chicken", "beef", "lamb"]
-console.log(array)
-var resultsArr = []
-var array = testArray1
-
-beefCount = 0
-chickenCount = 0
-fishCount = 0 
-var count = 0;
-
-for(var i = 0; i < array.length; ++i){
-    if(array[i] === 'beef'){
-        beefCount++;}
-    if (array[i] === 'chicken'){
-        chickenCount++;}
-    if (array[i] === 'fish'){
-        fish++;}
-}
-if(beefCount > chickenCount && beefCount > lambCount){
-    console.log("beefCount wins!")
-}
-if(chickenCount > beefCount && chickenCount > lambCount){
-    console.log("chickenCount wins!")
-}
-if(fishCount > chickenCount && lambCount > beefCount){
-    console.log("fishCount wins!")
-}
-
-
-//Make profiles of users
-//They get a score
-//Match them with a profile based on score
-//Each profile gets one recipe, one drink, one music
-var currentProfile;
-
-function loadMatchProfile(score) {
-    if (score > 0 && score < 10){
-        currentProfile = profiles.one
-    }
-    else if (score > 11 && score < 20){
-        currentProfile = profiles.two
+        chosenIngredient = "chicken"
+        chosenAlcohol = "rum"
     }
 }
 
-var profiles = {
-    one:{
-        ingredients: ["onions","chicken"]
-    },
-    two:{
-        ingredients: ["beef", "garlic"]
-    },
-    three:{
-        ingredients: ["peppers", "fish"]
-    },
-}
-
-function createRecipePuppyURL(ingredients) {
-    return `http://www.recipepuppy.com/api/?i=${ingredients.join(",")}`
+function score(food, number) {
+    foodArray[number] = food
+    console.log(foodArray)
 }
 
 function fetchRecipes(ingredients) {
+    console.log("fetchRecipes()started")
+    console.log(ingredients)
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -295,65 +81,27 @@ function fetchRecipes(ingredients) {
     return $.ajax(settings)
 }
 
-fetchRecipes(currentprofile.ingredients)
-    .then(function(response){
-       return JSON.parse(response)
-    })
-    .then(data => {
-        console.log(data)
-    })
+function GetDrink(baseAlcohol) {
+    console.log("Start Function GetDrink()")
+    //Cocktail API
+    baseAlcohol = baseAlcohol ? baseAlcohol : chosenAlcohol;
+    console.log(baseAlcohol)
 
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + baseAlcohol
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        crossDomain: true,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        DrinkRandom = Math.floor(Math.random() * response.drinks.length)
+        console.log(response.drinks[DrinkRandom])
+        $("#drinkImg").attr("src", response.drinks[DrinkRandom].strDrinkThumb);
+        console.log(response.drinks[DrinkRandom].strDrinkThumb)
+        $("#drinkTitle").text(response.drinks[DrinkRandom].strDrink);
+        /* $("#cocktailLink").text(jsonResponse.drinks[DrinkRandom].href); */
 
-// added content
+    });
 
-// function CitySearch(scoreTotal){
-//     console.log("Start City Search")
-//     var searchValue = $( "#SearchBox" ).val()
-//     console.log(scoreY1)
-//     console.log(scoreTotal)
-    
-//     var settings = {
-//       "async": true,
-//       "crossDomain": true,
-//       "url": "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=" + searchValue,
-//       "method": "GET",
-//       "headers": {
-//         "x-rapidapi-host": "tasty.p.rapidapi.com",
-//         "x-rapidapi-key": "3eb55b5bc9mshcd18231da3d1013p112c4cjsn6acd3db0b1c9"
-//       }
-//     }
-    
-//     $.ajax(settings).done(function (response) {
-//       console.log(response);
-//     });
-
-//     var settings = {
-//       "async": true,
-//       "crossDomain": true,
-//       "url": "https://tasty.p.rapidapi.com/tags/list",
-//       "method": "GET",
-//       "headers": {
-//         "x-rapidapi-host": "tasty.p.rapidapi.com",
-//         "x-rapidapi-key": "3eb55b5bc9mshcd18231da3d1013p112c4cjsn6acd3db0b1c9"
-//       }
-//     }
-    
-//     $.ajax(settings).done(function (response) {
-//       console.log(response);
-//     });
-// }
-
-// CitySearch()
-
-//Cocktail API
-/* console.log(searchValue)
-    
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+ searchValue 
-console.log(queryURL)
-$.ajax({
-  url: queryURL,
-  crossDomain: true,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-}); */
+}
